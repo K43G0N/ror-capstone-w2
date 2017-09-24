27f5a5b6195b014be6401e95ec50d23dfa8b5080
carlos eduardo gonçalves <kherokuone@rediffmail.com>
2017-09-24 18:27:14 -0300

    jkj

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,25 +1,17 @@
 class CitiesController < ApplicationController
   before_action :set_city, only: [:show, :update, :destroy]
 
-  # GET /cities
-  # GET /cities.json
   def index
     @cities = City.all
-
     render json: @cities
   end
 
-  # GET /cities/1
-  # GET /cities/1.json
   def show
     render json: @city
   end
 
-  # POST /cities
-  # POST /cities.json
   def create
     @city = City.new(city_params)
-
     if @city.save
       render json: @city, status: :created, location: @city
     else
@@ -27,8 +19,6 @@ class CitiesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /cities/1
-  # PATCH/PUT /cities/1.json
   def update
     if @city.update(city_params)
       head :no_content
@@ -37,8 +27,6 @@ class CitiesController < ApplicationController
     end
   end
 
-  # DELETE /cities/1
-  # DELETE /cities/1.json
   def destroy
     @city.destroy
 
@@ -52,6 +40,6 @@ class CitiesController < ApplicationController
     end
 
     def city_params
-      params.require(:city).permit(:name, :–orm, :active_directory)
+      params.require(:city).permit(:name)
     end
 end
